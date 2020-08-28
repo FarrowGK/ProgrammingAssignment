54b03df6e4e5098887c5baca55550fe96193bbc9
Gabriel Farrow <g.kahlilfarrow@gmail.com>
2020-08-28 13:19:29 -0400

    Rendering JSON data onto Views

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 const ProgrammingAssignment = (props) => {
-  const [users, getUsers] = useState([]);
+  const [users, getUsers] = useState([
+    {
+      address: {
+        city: '',
+        geo: {lat: '', lng: ''},
+        street: '',
+        suite: '',
+        zipcode: '',
+      },
+      company: {
+        bs: '',
+        catchPhrase: '',
+        name: '',
+      },
+      email: '',
+      id: -1,
+      name: '',
+      phone: '',
+      username: '',
+      website: '',
+    },
+  ]);
 
   const getUsersFromApiHandler = async () => {
     try {
@@ -16,7 +37,8 @@ const ProgrammingAssignment = (props) => {
 
   return (
     <View style={styles.container}>
-      <Text> {users[0].address.street} </Text>
+      <Text> Hello and Welcome To my React Native App </Text>
+      <Text>{users[0].name}</Text>
       <Button
         title="Click Me"
         onPress={() => {
